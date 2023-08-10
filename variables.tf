fc2d9973d25935e34be39647b13e3d2ac1707af1
md-shabbir <mohd.shabbir@oneconvergence.com>
2023-08-10 10:25:17 +0000

    initial commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "ami" {
+  type = string
+  default = "ami-2757f631"
+}
+
+variable "name" {
+  type = string
+  default = "sbr-test"
+}
